63e657c3e89415022218c2d71c478ec4c3fd6628
kolodziejgrzegorz <grzegorz.kolodziej.89@gmail.com>
2018-09-05 22:33:02 +0200

    Initial Project Add

---
+++ src/main/java/guru/spring/petclinic/SpringGuruPetClinicApplication.java
@@ -0,0 +1,12 @@
+package guru.spring.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuruPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuruPetClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/guru/spring/petclinic/SpringGuruPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.spring.petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringGuruPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
