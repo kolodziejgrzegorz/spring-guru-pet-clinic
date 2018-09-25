0af21a541c6e72ec3aa27ef77c0c8b65d24e0bbf
kolodziejgrzegorz <grzegorz.kolodziej.89@gmail.com>
2018-09-25 22:04:20 +0200

    Adding static resources

--- pet-clinic-web/src/main/java/guru/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/spring/petclinic/bootstrap/DataLoader.java
@@ -22,13 +22,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Jan");
         owner1.setLastName("Kowalski");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Pan");
         owner2.setLastName("Nowak");
         ownerService.save(owner2);
@@ -36,13 +34,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("owners loaded");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Jessie");
         vet1.setLastName("James");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Walter");
         vet2.setLastName("White");
         vetService.save(vet2);
